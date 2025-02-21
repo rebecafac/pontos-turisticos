edceba9121c88be25d27d25c7fd71552c601b046
Rebeca Facchinato <rebecafac@hotmail.com>
2025-02-21 18:20:58 -0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'
+import { Component } from '@angular/core'
+
+import {
+    PoMenuItem,
+    PoMenuModule,
+    PoPageModule,
+    PoToolbarModule,
+} from '@po-ui/ng-components'
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [CommonModule, PoToolbarModule, PoMenuModule, PoPageModule],
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pontos-turisticos';
+    readonly menus: Array<PoMenuItem> = [
+        { label: 'Home', action: this.onClick.bind(this) },
+    ]
+
+    private onClick() {
+        alert('Clicked in menu item')
+    }
 }
