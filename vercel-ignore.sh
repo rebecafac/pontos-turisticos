17621f1be0767f1d079d345cfe08f27c41d9a749
Rebeca Facchinato <rebecafac@hotmail.com>
2026-03-18 12:39:41 -0300

    ajuste na configuração de geração de build para o vercel

--- vercel-ignore.sh
+++ vercel-ignore.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-
-if [ "$VERCEL_GIT_COMMIT_REF" != "main" ]; then
-  echo "⛔ Ignorando build da branch $VERCEL_GIT_COMMIT_REF"
-  exit 0
-fi
-
-echo "✅ Permitindo build"
+echo "Permitindo build"
 exit 1
